6f34194768acd58c54743c179f03acaf58120f72
meenheepark <49273568+meenheepark@users.noreply.github.com>
2020-05-11 23:52:21 +0900

    down-up  2xN 타일 채우는 방법의 수

---
+++ DP/11726.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/*
+사이트 : 백준
+문제번호 : 11726
+분류 : 다이나믹프로그래밍
+설명 : 2xN 타일 채우는 방법의 수 d[n] = d[n-1] + d[n-2]
+*/
+int d[1001];
+
+int main() {
+	int n;
+	cin >> n;
+	d[1] = 1;
+	d[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		d[i] = (d[i - 1] + d[i - 2]) % 10007;
+	}
+	cout << d[n];
+}
