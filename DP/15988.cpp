755fdfe8c08cde9592cf29ec983c023bd7ab9ded
meenheepark <49273568+meenheepark@users.noreply.github.com>
2020-05-12 00:21:39 +0900

    down up 1,2,3더하기 범위 엄청커짐
    배열을 long long으로 선언했더니 맞음.
    1,000,000,009로 나눠준 것을 저장해야 하기 때문인듯

---
+++ DP/15988.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+/*
+사이트 : 백준
+문제번호 : 9095
+분류 : 다이나믹프로그래밍
+설명 : 1,2,3 더하기  3 d[n] = d[n-1] + d[n-2] + d[n-3]
+숫자 제한이 커졌음
+*/
+long long d[1000001]; // 여기를 롱롱으로 써줘야함
+
+int main() {
+	int n;
+	cin >> n;
+	for (int k = 0; k < n; k++) {
+		int r;
+		cin >> r;
+		d[0] = 1; // 1,2,3 모두 안사용 하는 방법의 수 1 
+		d[1] = 1;
+		d[2] = 2;
+		for (int i = 3; i <= r; i++) {
+			d[i] = (d[i - 1] + d[i - 2] + d[i - 3]) % 1000000009;
+		}
+		cout << d[r]<< endl;
+	}
+}
