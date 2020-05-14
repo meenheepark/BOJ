3bf3aa298588b57493b178103dc473935d639d2a
meenheepark <49273568+meenheepark@users.noreply.github.com>
2020-05-14 11:22:56 +0900

    앞을 전수 조사한다는 특징이 있음

---
+++ DP/11053.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+사이트 : 백준
+문제번호 : 11053
+분류 : 다이나믹프로그래밍
+설명 : 가장 긴 증가하는 부분수열
+점화식 : d[i] = max(d[j])+1
+dp로 풀려면 그 전까지 답이 최대, 즉 j까지 답이 정답이어야 함
+
+*/
+
+int a[1001];
+int d[1001];
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < n; i++) {
+		d[i] = 1; // 자신이 처음으로 포함될 경우
+		for (int j = 0; j < i; j++) {
+			if (a[j] < a[i] && d[i] < d[j] + 1) { // 최대로 포함되는수.나보다 더 작고 그것을 포함한 길이가 더 클때
+				d[i] = d[j]+1;
+			}
+		}
+	}
+	int maxx = 0;
+	for (int i = 0; i < n; i++) {
+		if (maxx < d[i]) maxx = d[i];
+	}
+	cout << maxx;
+}
